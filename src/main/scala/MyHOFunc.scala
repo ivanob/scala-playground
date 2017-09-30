f823873f2cf66aa5201588e2d7f8ebf2393fa4d9
Ivan Obeso <ivan.obeso1@gmail.com>
2017-09-30 11:56:06 +0100

    added 2 more functions. TODO: one test is broken

--- src/main/scala/MyHOFunc.scala
+++ src/main/scala/MyHOFunc.scala
@@ -1,10 +1,37 @@
-
+/**
+  * This class contains my own implementation of the high order functions
+  * available in scala. It is coded for educational purposes so maybe they
+  * are not the most efficient implementations.
+  */
 class MyHOFunc[A](val l: List[A]){
   import MyHOFunc._
 
   def myMap(f:A=>A): List[A] = {
     (for{i<-0 to l.length-1} yield f(l(i))).toList
   }
+
+  def myZip[B](l2:List[B]):List[(A,B)] = {
+    (for { i<-0 to l.length-1 if(l2.length>i)} yield (l(i),l2(i))).toList
+  }
+
+  def myFind(f:Int => Boolean): Option[A] = {
+    (for{
+      i<- 0 to l.length-1
+      if(f(i))}
+      yield l(i)).toList
+        match {
+         case x::xs => Some(x)
+         case x::Nil => Some(x)
+         case Nil => None
+      }
+  }
+
+  def myReduce(f:(A,A)=>A): A = l match {
+    case x :: Nil => x
+    case x :: xs => f(x,xs.myReduce(f))
+    case Nil => throw new Exception("ERROR")
+  }
+
 }
 
 object MyHOFunc {
--- src/test/scala/MyHOFuncSuite.scala
+++ src/test/scala/MyHOFuncSuite.scala
@@ -6,9 +6,25 @@ import MyHOFunc._
 @RunWith(classOf[JUnitRunner])
 class MyHOFuncSuite extends FunSuite{
 
-  test("test of the map function"){
+  test("test of the MAP function"){
     assert(List(1,2,3).myMap(x => x) == List(1,2,3)) //Identity function
     assert(List(1,2,3).myMap(x => x*2) == List(2,4,6))
     assert(List(1,2,3,4,5).myMap(x => x*x) == List(1,4,9,16,25))
+  }
+
+  test("test of ZIP function"){
+    assert(List(1,2,3).myZip(List('a,'b,'c)) == List((1,'a),(2,'b),(3,'c)))
+    assert(List(1,2,3).myZip(List('a)) == List((1,'a))) //Unpair
+    assert(List(1).myZip(List('a,'b,'c)) == List((1,'a))) //Unpair
+  }
+
+  test("test of FIND function"){
+    assert(List(1,2,3,4,5).myFind(x=>x==1) == Some(0))
+  }
+
+  test("test of REDUCE function"){
+    assert(List(1).myReduce((a,b)=>a+b) == 1)
+    assert(List(1,2).myReduce((a,b)=>a+b) == 3)
+    assert(List(1,2,3).myReduce((a,b)=>a+b) == 6)
   }
 }
