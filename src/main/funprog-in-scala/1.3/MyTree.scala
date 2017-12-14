65b1ce0691462b2e28f1fa34bf36b8da2f9f73fd
Ivan Obeso <ivan.obeso1@gmail.com>
2017-12-14 22:55:54 +0000

    added a couple of exercises

--- src/main/funprog-in-scala/1.3/MyTree.scala
+++ src/main/funprog-in-scala/1.3/MyTree.scala
@@ -5,9 +5,34 @@ object MyTree {
   case class Leaf[A](value: A) extends Tree[A]
 
   object Tree{
-      def size[A](t: Tree[A]):Int = t match {
-        case Branch(l,r) => size(l) + size(r)
-        case Leaf(_) => 1
+    //Exercise 3.25
+    def size[A](t: Tree[A]):Int = t match {
+      case Branch(l,r) => size(l) + size(r)
+      case Leaf(_) => 1
+    }
+
+    //Exercise 3.26
+    def maximum(t: Tree[Int]): Int = t match {
+      case Branch(l,r) => maximum(l) max maximum(r)
+      case Leaf(x) => x
+    }
+    //Exercise 3.27
+    def depth[A](t: Tree[A]):Int = t match {
+      case Branch(l,r) => {
+        val leftDepth = depth(l)
+        val rightDepth = depth(r)
+        if(leftDepth>rightDepth) leftDepth + 1 else rightDepth + 1
       }
+      case Leaf(x) => 1
+    }
+
+    //Exercise 3.28
+    def map[A,B](t: Tree[A])(f: A=>B): Tree[B] = t match {
+      case Branch(l,r) => Branch(map(l)(f),map(r)(f))
+      case Leaf(x) => Leaf(f(x))
+    }
+
+    //Exercise 3.29
+    def fold()
   }
 }
--- src/test/funprog-in-scala/1.3/TreeSuite.scala
+++ src/test/funprog-in-scala/1.3/TreeSuite.scala
@@ -9,8 +9,15 @@ class TreeSuite extends FunSuite {
   test("test of the size function") {
     val tree1 = Branch(Leaf(1),Leaf(2))
     val tree2 = Branch(Branch(Leaf(1),Leaf(2)),Branch(Leaf(3),Leaf(4)))
+    val tree3 = Branch(Branch(Branch(Branch(Leaf(1),Leaf(1)),Leaf(1)),Leaf(1)),Leaf(1))
     assert(Tree.size(tree1) == 2)
     assert(Tree.size(tree2) == 4)
+    assert(Tree.maximum(tree1) == 2)
+    assert(Tree.maximum(tree2) == 4)
+    assert(Tree.depth(tree1) == 2)
+    assert(Tree.depth(tree2) == 3)
+    assert(Tree.depth(tree3) == 5)
+    assert(Tree.map(tree2)((a:Int) => a.toString) == Branch(Branch(Leaf("1"),Leaf("2")),Branch(Leaf("3"),Leaf("4"))))
 
   }
 
